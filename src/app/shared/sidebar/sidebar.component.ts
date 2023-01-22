230aa6e05321701a7963ef20ae2b8b8b745c575f
elkin-garzon <elkingarzonyaya@gmail.com>
2023-01-22 15:01:14 -0500

    ajusts for responsive

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StoreService } from 'src/app/services/store.service';
 
 
 @Component({
@@ -10,11 +11,21 @@ export class SidebarComponent {
 
 	public active:number = 0;
 
+	constructor(
+		private store: StoreService
+	) {
+
+	}
+
 	activeLink(index:number){
 		if(this.active !== index){
 			this.active = index;
 		}else{
 			this.active = 0;
 		}
+	}
+
+	changeStatusMenu() {
+		this.store.changeMenu();
 	}
 }
